ce7fe54036dc0060353c21594887ae258aae3758
kether chau <ketherchau@gmail.com>
2021-05-03 15:51:10 +0800

    Update client/next.config.js

---
+++ client/next.config.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv")
+dotenv.config();
+
+module.exports = {
+  webpack: (config, { buildId, dev }) => {
+    // This allows the app to refer to files through our symlink
+    config.resolve.symlinks = false
+    return config
+  },
+  env: {
+    INFURA_ID: process.env.INFURA_ID,
+    MAINNET_ID: process.env.MAINNET_ID,
+    RINKEBY_ID: process.env.RINKEBY_ID,
+    ROPSTEN_ID: process.env.ROPSTEN_ID
+  }
+}
