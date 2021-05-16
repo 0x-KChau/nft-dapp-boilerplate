2ba7d9655ded80a2202024b8a7ff8d1d7fb0f6a5
kether chau <ketherchau@gmail.com>
2021-05-16 13:17:15 +0800

    Update openGraph image

--- client/src/next-seo.config.js
+++ client/src/next-seo.config.js
@@ -5,6 +5,20 @@ export default {
       site_name: 'SHIBA NFT',
       title: 'SHIBA NFT, buy and keep an adorable doge home.',
       description: 'SHIBA is one of the most fantastic dogs in the universe. This project aims to bring adorable SHIBAs to the Ethereum network with only a limited number of 1000 units.',
+      images: [
+        {
+          url: 'https://www.shibanft.io/favicon.ico',
+          width: 400,
+          height: 400,
+          alt: 'SHIBA favicon ico',
+        },
+        {
+          url: 'https://www.shibanft.io/static/favicon.png',
+          width: 400,
+          height: 400,
+          alt: 'SHIBA favicon png',
+        },
+      ],
     },
     twitter: {
       handle: '@SHIBA_NFT',
